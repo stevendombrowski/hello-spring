0045936c1463360f27b1d97f4de2c0ebc9314483
stevendombrowski <88370598+stevendombrowski@users.noreply.github.com>
2024-05-02 01:45:13 -0400

    Started lesson

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,43 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+    //handle request at path http://localhost:8080/hello
+
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello, Spring!";
+//    }
+
+    //handle request at path //http://localhost:8080/goodbye
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodbye(){
+        return "Goodbye, Spring!";
+    }
+
+    //handle requests of the form http://localhost:8080/hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name){
+        return "Hello, " + name + "!";
+    }
+
+    //handle requests of the form http://localhost:8080/hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("form")
+//    @ResponseBody //with templates @ResponseBody is not needed
+    public String helloForm() {
+        return "form";
+    }
+
+}
